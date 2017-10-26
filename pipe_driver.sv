3f7d6e3171862ab0eebc862a34de6ebd9daa9093
Amit Kumar <Amit.s.kumar@outlook.com>
2017-10-25 23:42:28 -0700

    Added transfer packet and driver

---
+++ pipe_driver.sv
@@ -0,0 +1,72 @@
+class pipe_driver extends uvm_driver #(transfer);
+   virtual pipe_if vif;
+
+   `uvm_component_utils(pipe_driver)
+
+   function new(string name, uvm_component parent);
+      super.new(name);
+   endfunction // new
+
+   function void build_phase(uvm_phase phase);
+      super.build_phase(phase);
+      if (!uvm_config_db#(virtual pipe_if)::get(this, "", "in_intf",vif))
+	`uvm_fatal("NOVIF", {"Virtual interface must be set for: ",get_full_name( ), ".vif"})
+      `uvm_info(get_full_name( ), "Build stage complete.", UVM_LOW)
+   endfunction // build_phase
+
+   virtual task run_phase(uvm_phase phase);
+      fork
+	 reset();
+	 get_and_drive();
+      join
+   endtask // run_phase
+
+   virtual task reset();
+      forever begin
+	 @(negedge vif.rst_n);
+	 `uvm_info(get_type_name(), " Resetting signals ", UVM_LOW)
+	 vif.cf = 2'b0;
+	 vif.data_in0 = 16'b0;
+	 vif.data_in1 = 16'b0;
+	 vif.enable   = 1'b0;
+      end
+    endtask // reset
+
+   virtual task get_and_drive() ;
+      forever begin
+	 @(posedge vif.rst_n);
+	 while(vif.rst_n != 1'b0) begin
+	    seq_item_port.get_next_item(req);
+	    driver_packet(req);
+	    seq_item_port.item_done(req);
+	 end
+      end
+   endtask // get_and_drive
+
+   virtual task drive_packet(transfer pkt);
+      vif.enable = 1'b0;
+      repeat (pkt.delay) @(posedge vif.clk);
+      vif.enable    = pkt.enable;
+      vif.data_in0  = pkt.data_in0;
+      vif.data_in1 = pkt.data_in1;
+      vif.cf = pkt.cf;
+      @(posedge vif.clk);
+      vif.enable = 1'b0;
+   endtask // drive_packet
+endclass // pipe_driver
+
+
+      
+
+      
+   
+
+      
+      
+
+      
+
+      
+      
+      
+   
---
+++ transfer.sv
@@ -0,0 +1,38 @@
+class transfer extends uvm_sequence_item;
+   rand bit [1:0]  cf;
+   rand bit        enable;
+   rand bit [15:0] data_in0;
+   rand bit [15:0] data_in1;
+   rand bit [15:0] data_out0;
+   rand bit [15:0] data_out1;
+   rand bit        delay;
+
+   constraint add_delay { delay inside {[0:5]};}
+
+   `uvm_object_utils_begin(transfer)
+       `uvm_field_int( cf       ,UVM_DEFAULT)
+       `uvm_field_int( enable   ,UVM_DEFAULT)
+       `uvm_field_int( data_in0 ,UVM_DEFAULT)
+       `uvm_field_int( data_in1 ,UVM_DEFAULT)
+       `uvm_field_int( data_out0,UVM_DEFAULT)
+       `uvm_field_int( data_out1,UVM_DEFAULT)
+       `uvm_field_int( delay    ,UVM_DEFAULT)
+    `uvm_object_utils_end
+
+   function new(string name="data_packet");
+      super.new(name);
+   endfunction: new
+
+   virtual task displayAll();
+      `uvm_info("DP",$formatf("cf=%0h enable=%0b data_in0=%0h data_in1=%0h data_out0=%0h data_out1=%0h delay=%0d",cf,enable, data_in0, data_in1,  data_out0, data_out1,delay), UVM_LOW);
+   endtask : displayAll
+
+endclass // transfer
+
+
+      
+
+
+
+      
+	     
