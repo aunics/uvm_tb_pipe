2e20913f6c7df489dae5e0951d0d6714db204168
aunics <32985709+aunics@users.noreply.github.com>
2017-11-17 01:31:33 -0600

    Create pipe_pkg.sv

---
+++ pipe_pkg.sv
@@ -0,0 +1,11 @@
+package pipe_pkg;
+   import uvm_pkg::*;
+`include "uvm_macros.svh"
+`include "transfer.sv" 
+`include "pipe_driver.sv" 
+`include "pipe_monitor.sv" 
+`include "pipe_sequencer.sv" 
+`include "pipe_scoreboard.sv" 
+`include "pipe_env.sv" 
+`include "dut_env.sv" 
+endpackage // pipe_pkg
