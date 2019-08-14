f31c7b4e4d9f926aae6e080f37cd15c3c52fe6e1
amit kumar <amit.s.kumar@outlook.com>
2019-08-14 12:33:48 -0500

    design updates
    Minor bug fixes in the design -were mostly copy/paste errors.

--- dut/design.sv
+++ dut/design.sv
@@ -1,11 +1,11 @@
-module pipe{ clk, 
+module pipe( clk, 
              rst_n,
              i_cf,
              i_en,
              i_data0,i_data1,
              o_data0,
              o_data1
-};
+           );
 
    input        clk;
    input        rst_n;
@@ -24,35 +24,36 @@ module pipe{ clk,
    wire [15:0] i_data0;
    wire [15:0] i_data1;
 
-   reg [15:0]  o_data0;
+  reg [15:0]   o_data0;
    reg [15:0]  o_data1;
    
    always @(posedge clk) begin
       if (!rst_n) begin
-	 data_0 <= 16'h0000;
-         data_1 <= 16'h0000;
+	     o_data0 <= 16'h0000;
+         o_data1 <= 16'h0000;
       end
       else begin
 	 if (i_en) begin
 	    if (i_data0 == 16'h0000 || i_data0 == 16'hFFFF) begin
-	       data_0 <= i_data0;
+	       o_data0 <= i_data0;
 	    end
 	    else
-	      data_0 <= i_data0 * i_cf;
+	      o_data0 <= i_data0 * i_cf;
 	 end
 	 if (i_en) begin
 	    if (i_data1 == 16'h0000 || i_data1 == 16'hFFFF) begin
-	       data_1 <= i_data1;
+	       o_data1 <= i_data1;
 	    end
 	    else begin
-	       data_1 <= i_data1 * i_cf;
+	       o_data1 <= i_data1 * i_cf;
 	    end
 	 end
       end // else: !if(!rst_n)
    end // always @ (posedge clk)
 
    always @(posedge clk) begin
-      o_data0 <= data_0;
-      o_data1 <= data_1;
+      o_data0 <= o_data0;
+      o_data1 <= o_data1;
    end
+  
 endmodule
